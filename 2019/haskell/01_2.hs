175834d2365b68f32f17a3f379c2f91c6a29aa6e
nkzlkk <snowcrash@tuta.io>
2019-12-02 17:31:42 +0100

    day one solutions in haskell

---
+++ 2019/haskell/01_2.hs
@@ -0,0 +1,11 @@
+import System.IO 
+
+revCount :: Int -> Int
+revCount n = if n > 0 then n + revCount ( (n `div` 3) - 2) else 0
+
+fuelCount :: [String] -> Int
+fuelCount = foldr (+) 0 . map (revCount . (\x -> x `div` 3 - 2) . read) 
+
+main = do
+    y <- readFile "2019/01.txt"
+    print (fuelCount (lines y))

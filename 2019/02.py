b512451c644453a5413fe4cd1dbc064db48fde45
nkzlkk <snowcrash@tuta.io>
2019-12-02 08:13:21 +0100

    second

---
+++ 2019/02.py
@@ -0,0 +1,43 @@
+from copy import deepcopy
+
+def opcodes(filename):
+    with open(filename, "r") as f:
+        line = f.read().strip()
+    lines = [int(i) for i in line.split(",")]
+    
+    lines[1] = 12
+    lines[2] = 2
+    return foo(lines)
+
+    # for i in range(0, len(lines), 4):
+    #     if lines[i] == 1:
+    #         lines[lines[i+3]] = lines[lines[i+1]] + lines[lines[i+2]]
+    #     elif lines[i] == 2:
+    #         lines[lines[i+3]] = lines[lines[i+1]] * lines[lines[i+2]]
+
+    # return lines
+
+def foo(lines):
+    for i in range(0, len(lines), 4):
+        if lines[i] == 1:
+            lines[lines[i+3]] = lines[lines[i+1]] + lines[lines[i+2]]
+        elif lines[i] == 2:
+            lines[lines[i+3]] = lines[lines[i+1]] * lines[lines[i+2]]
+
+    return lines
+
+def output(filename):
+    with open(filename, "r") as f:
+        line = f.read().strip()
+    lines = [int(i) for i in line.split(",")]
+
+    for i in range(100):
+        for j in range(100):
+            arr = deepcopy(lines)
+            arr[1] = i
+            arr[2] = j
+            print("here")
+            if foo(arr)[0] == 19690720:
+                return 100 * i + j
+
+    

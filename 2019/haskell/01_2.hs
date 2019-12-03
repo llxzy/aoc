e402cb989441fdbddfefdbf5af2e591b94df628f
nkzlkk <snowcrash@tuta.io>
2019-12-03 17:49:24 +0100

    refactor haskell first

--- 2019/haskell/01_2.hs
+++ 2019/haskell/01_2.hs
@@ -6,6 +6,7 @@ revCount n = if n > 0 then n + revCount ( (n `div` 3) - 2) else 0
 fuelCount :: [String] -> Int
 fuelCount = foldr (+) 0 . map (revCount . (\x -> x `div` 3 - 2) . read) 
 
+main :: IO ()
 main = do
     y <- readFile "2019/01.txt"
     print (fuelCount (lines y))

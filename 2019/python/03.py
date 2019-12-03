8112b8d10e052d2eb067bfa1c63d121f5cb7494b
nkzlkk <snowcrash@tuta.io>
2019-12-03 16:34:36 +0100

    third

---
+++ 2019/python/03.py
@@ -0,0 +1,52 @@
+def get_input(filename):
+    with open(filename, "r") as f:
+        lines = [i.strip().split(",") for i in f.readlines()]
+    return lines
+
+def dist(a, b):
+    return abs(a[0] - b[0]) + abs(b[1] - a[1])
+
+def part1(filename):
+    lines = get_input(filename)
+    center = (0, 0)
+    temp = []
+    temp2 = []
+    moveX = {"D": 0, "U": 0, "L": -1, "R": 1}
+    moveY = {"D": -1, "U": 1, "L": 0, "R": 0}
+    for wire in lines:
+        last = (0, 0)
+        wire_points = set()
+        inter_steps = {}
+        stepcounter = 1
+        for cmd in wire:
+            drc = cmd[0]
+            steps = int(cmd[1:])
+            for i in range(steps):
+                x = (last[0]+moveX[drc], last[1]+moveY[drc])
+                wire_points.add(x)
+                inter_steps[x] = stepcounter
+                stepcounter += 1
+                last = x
+        temp.append(wire_points)
+        temp2.append(inter_steps)
+
+    intersections = set.intersection(*temp)
+    workset = [dist(center, i) for i in intersections]
+    print("intersections", intersections)
+
+    out = {}
+    for a in temp2:
+        for i in a:
+            if i in intersections:
+                print(i)
+                out[i] = out.get(i, 0) + a[i]
+
+    result = []
+    for i in out:
+        result.append(out[i])
+
+
+    #part1
+    # return min(workset)
+    #part2
+    return min(result)
